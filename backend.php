2b258c084e4a0a40ef36dfbd32975384ec8e0b2b
mackliu <macklun@gmail.com>
2021-01-22 13:41:48 +0800

    完成管理者登入權限選單顯示功能

--- backend.php
+++ backend.php
@@ -1,4 +1,14 @@
-<?php include_once "base.php";?>
+<?php include_once "base.php";
+
+if(empty($_SESSION['admin'])){
+    to("index.php");
+    exit();
+}else{
+    $admin=$Admin->find(['acc'=>$_SESSION['admin']]);
+    $pr=unserialize($admin['pr']);
+}
+
+;?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0057)?do=admin -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -21,12 +31,13 @@
         <div id="left" class="ct">
         	<div style="min-height:400px;">
         	    <a href="?do=main">管理權限設置</a>
-            	<a href="?do=th">商品分類與管理</a>
-            	<a href="?do=order">訂單管理</a>
-            	<a href="?do=mem">會員管理</a>
-            	<a href="?do=bot">頁尾版權管理</a>
-            	<a href="?do=main">最新消息管理</a>
-            	<a href="?do=logout" style="color:#f00;">登出</a>
+                <?=(in_array(1,$pr))?"<a href='?do=th'>商品分類與管理</a>":''; ?>
+                <?=(in_array(2,$pr))?"<a href='?do=order'>訂單管理</a>":''; ?>
+                <?=(in_array(3,$pr))?"<a href='?do=mem'>會員管理</a>":'' ?>
+                <?=(in_array(4,$pr))?"<a href='?do=bot' >頁尾版權管理</a>":'' ?>
+                <?=(in_array(5,$pr))?"<a href='?do=main' >最新消息管理</a>":'' ?>
+                
+            	<a href="javascript:lof('api/logout.php?do=admin')" style="color:#f00;">登出</a>
                     </div>
                     </div>
         <div id="right">
