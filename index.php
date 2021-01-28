901d3fff10cf8287fb1fedeb50b3f506f33fffd2
mackliu <macklun@gmail.com>
2021-01-28 11:41:31 +0800

    前台顯示商品分類選單

--- index.php
+++ index.php
@@ -42,8 +42,35 @@
         </div>
         <div id="left" class="ct">
         	<div style="min-height:400px;">
-        	            </div>
-                        <span>
+                    <!--選單-->
+                <a href='?big=0'>全部商品(<?=$Goods->count();?>)</a>
+            <?php
+                $bigs=$Type->all(['parent'=>0]);
+                foreach($bigs as $big){
+            ?>
+                <div class='ww'><a  href='?big=<?=$big['id'];?>'><?=$big['name'];?>(<?=$Goods->count(['big'=>$big['id']]);?>)</a>
+            <?php
+
+                    $mids=$Type->all(['parent'=>$big['id']]);
+                    if(count($mids)>0){
+                    echo "<div class='s'>";
+                        foreach($mids as $mid){
+                        ?>
+                            <a  href='?big=<?=$big['id'];?>&mid=<?=$mid['id'];?>'><?=$mid['name'];?>(<?=$Goods->count(['mid'=>$mid['id']]);?>)</a>
+                        <?php
+                        }
+                    echo "</div>";
+                    }
+
+                    echo "</div>";
+                }
+
+
+            ?>
+
+
+        	</div>
+            <span>
             	<div>進站總人數</div>
                 <div style="color:#f00; font-size:28px;">
                 	00005                </div>
