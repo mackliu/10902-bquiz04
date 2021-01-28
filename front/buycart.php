3177dbd33249bfe78c38ff81e1d8ebaccb71ef6b
mackliu <macklun@gmail.com>
2021-01-28 15:32:10 +0800

    完成購物車功能

--- front/buycart.php
+++ front/buycart.php
@@ -1,1 +1,48 @@
-購物車
+<?php
+
+if(isset($_GET['goods'])){
+    $_SESSION['cart'][$_GET['goods']]=$_GET['qt'];
+}
+
+if(empty($_SESSION['mem'])){
+    to("../index.php?do=login");
+    exit();
+}
+
+?>
+
+<h2 class="ct"><?=$_SESSION['mem'];?>的購物車</h2>
+
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>數量</td>
+        <td>庫存量</td>
+        <td>單價</td>
+        <td>小計</td>
+        <td>刪除</td>
+    </tr>
+<?php
+    foreach ($_SESSION['cart'] as $id =>$qt) {
+        $g=$Goods->find($id);
+?>
+    <tr class="pp ct">
+        <td><?=$g['num'];?></td>
+        <td><?=$g['name'];?></td>
+        <td><?=$qt;?></td>
+        <td><?=$g['quota'];?></td>
+        <td><?=$g['price'];?></td>
+        <td><?=$qt*$g['price'];?></td>
+        <td><img src="icon/0415.jpg" ></td>
+    </tr>
+<?php
+    }
+?>
+</table>
+<div class="ct">
+    <a href="index.php"><img src="icon/0411.jpg" alt=""></a>
+    <a href="?do=checkout"><img src="icon/0412.jpg" alt=""></a>
+</div>
+
+
